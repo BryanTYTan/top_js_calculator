53363a9886d7157a03bc576cc7182d34e367a1d1
BryanTYTan <90520318+BryanTYTan@users.noreply.github.com>
2025-07-31 21:06:06 +0800

    Updated Logic

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,13 +1,18 @@
-let buttons = document.querySelectorAll('.cal-button')
+let buttons = document.querySelectorAll('.cal-button');
 
-button_box.addEventListener('click', (event) => {
-    const isButton = event.target.nodeName === 'BUTTON';
-    if (!isButton) {
-        return;
-    }
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return;
+        }
 
-    update_calc(event.target.value);
+        update_calc(event.target.value);
+    });
 });
+
+
+
 
 function update_calc(user_choice) {
     console.log(user_choice);
